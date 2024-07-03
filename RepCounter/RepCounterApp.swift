d42c7d319bc5e0b45721f153efb07d3a1b5f2e1c
MilanLabuss <milanlabus29@gmail.com>
2024-07-03 17:34:51 +0100

    Initial Commit

---
+++ RepCounter/RepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepCounterApp.swift
+//  RepCounter
+//
+//  Created by Milan Labus on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
